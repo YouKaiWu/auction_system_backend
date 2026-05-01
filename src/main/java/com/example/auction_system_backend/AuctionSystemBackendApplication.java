d21ada8d0c8f7cd78aeb12636a57b2d79764cd7d
YouKaiWu <ten920113@gmail.com>
2026-05-01 12:17:13 +0800

    feat: add db resource and mapper config path

--- src/main/java/com/example/auction_system_backend/AuctionSystemBackendApplication.java
+++ src/main/java/com/example/auction_system_backend/AuctionSystemBackendApplication.java
@@ -1,13 +1,14 @@
 package com.example.auction_system_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.auction_system_backend.mapper")
 public class AuctionSystemBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuctionSystemBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionSystemBackendApplication.class, args);
+    }
 }
